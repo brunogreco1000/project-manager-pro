4d83f302d3a80499bb5f8fb079b60df857c76029
brunogreco1000 <brunogreco1000@gmail.com>
2025-10-24 10:26:18 -0300

    Actualizar app.module.ts con cambios recientes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    ProjectsModule,
     // agrega ProjectsModule, TasksModule, ReportsModule
   ],
 })
